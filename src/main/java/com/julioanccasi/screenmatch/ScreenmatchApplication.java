9baf74d0df08e7fd9bacedd2928649a78d61f86e
Julio Anccasi <julio.anccasi.dev@gmail.com>
2024-07-03 19:53:31 -0500

    Screenmatch over

--- src/main/java/com/julioanccasi/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/julioanccasi/screenmatch/ScreenmatchApplication.java
@@ -1,17 +1,10 @@
 package com.julioanccasi.screenmatch;
 
-import com.julioanccasi.screenmatch.model.DatosEpisodio;
-import com.julioanccasi.screenmatch.model.DatosSerie;
-import com.julioanccasi.screenmatch.model.DatosTemporadas;
+import com.julioanccasi.screenmatch.principal.EjemploStreams;
 import com.julioanccasi.screenmatch.principal.Principal;
-import com.julioanccasi.screenmatch.service.ConsumoApi;
-import com.julioanccasi.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -24,5 +17,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Principal principal = new Principal();
 		principal.muestraElMenu();
+//		EjemploStreams ejemploStreams = new EjemploStreams();
+//		ejemploStreams.muestraEjemplo();
 	}
 }
--- src/main/java/com/julioanccasi/screenmatch/principal/Principal.java
+++ src/main/java/com/julioanccasi/screenmatch/principal/Principal.java
@@ -3,12 +3,14 @@ package com.julioanccasi.screenmatch.principal;
 import com.julioanccasi.screenmatch.model.DatosEpisodio;
 import com.julioanccasi.screenmatch.model.DatosSerie;
 import com.julioanccasi.screenmatch.model.DatosTemporadas;
+import com.julioanccasi.screenmatch.model.Episodio;
 import com.julioanccasi.screenmatch.service.ConsumoApi;
 import com.julioanccasi.screenmatch.service.ConvierteDatos;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -45,6 +47,75 @@ public class Principal {
 //            }
 //        }
 
-        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+//        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+//        Convertir todas las informaciones a una lista del tipo DatosEpisodio
+        List<DatosEpisodio> datosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+//        Top 5 episodios
+//        System.out.println("Top 5 episodios");
+//        datosEpisodios.stream()
+//                .filter(e -> !e.evaluacion().equalsIgnoreCase("N/A"))
+//                .peek(e -> System.out.println("Primer filtro (N/A)" + e))
+//                .sorted(Comparator.comparing(DatosEpisodio::evaluacion).reversed())
+//                .peek(e -> System.out.println("Segundo ordenación (M > m)" + e))
+//                .map(e -> e.titulo().toUpperCase())
+//                .peek(e -> System.out.println("Tercer filtro Mayúsculas (m > M)" + e))
+//                .limit(5)
+//                .forEach(System.out::println);
+
+//        Convirtiendo los datos a una lista del tipo Episodio
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d)))
+                .collect(Collectors.toList());
+
+//        episodios.forEach(System.out::println);
+
+//        Búsqueda de episodios a partir de x año
+//        System.out.println("Por favor indica el año a partir del cual deseas ver los episodios");
+//        var fecha = scanner.nextInt();
+//        scanner.nextLine();
+
+//        LocalDate fechaBusqueda = LocalDate.of(fecha, 1, 1);
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/YYYY");
+//        episodios.stream()
+//                .filter(e -> e.getFechaDeLanzamiento() != null && e.getFechaDeLanzamiento().isAfter(fechaBusqueda))
+//                .forEach(e -> System.out.println(
+//                        "Temporada " + e.getTemporada() +
+//                                "Episodio " + e.getTitulo() +
+//                                "Fecha de Lanzamiento " + e.getFechaDeLanzamiento().format(dtf)
+//                ));
+
+//        Busca episodios por un pedazo del titulo
+//        System.out.println("Por favor escriba el titulo del episodio que desea ver");
+//        var pedazoTitulo = scanner.nextLine();
+//        Optional<Episodio> episodioBuscado = episodios.stream()
+//                .filter(e -> e.getTitulo().toUpperCase().contains(pedazoTitulo.toUpperCase()))
+//                .findFirst();
+//
+//        if (episodioBuscado.isPresent()) {
+//            System.out.println("Episodio encontrado");
+//            System.out.println("Los datos son: " + episodioBuscado.get());
+//        } else {
+//            System.out.println("Episodio no encontrado");
+//        }
+
+        Map<Integer, Double> evaluacionesPorTemporada = episodios.stream()
+                .filter(e -> e.getEvaluacion() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada, Collectors.averagingDouble(Episodio::getEvaluacion)));
+
+        System.out.println(evaluacionesPorTemporada);
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getEvaluacion() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getEvaluacion));
+
+        System.out.println("Media de las evaluaciones: " + est.getAverage());
+        System.out.println("Episodio mejor evaluado: " + est.getMax());
+        System.out.println("Episodio peor evaluado: " + est.getMin());
     }
 }
